37f596642a1ed8f7733ec01dc7b60b7d9296b92c
OscarOK <oscareom1a@gmail.com>
2017-10-27 15:57:09 -0600

    ADDING COS FUNCTION
    Using Taylor Serie for cos.

--- TrigonometricFunctions.py
+++ TrigonometricFunctions.py
@@ -19,3 +19,16 @@ def sin(k, x):
         result += ((-1) ** n) / (math.factorial(2 * n + 1)) * (x ** (2 * n + 1))
 
     return result
+
+#TAYLOR SERIE OF COS
+def cos(k, x):
+
+    result = 1.0
+
+    for n in range(1, k):
+        result += ((-1) ** n) / (math.factorial(2 * n)) * (x ** (2 * n))
+
+    return result
+
+print (sin(40, math.pi))
+print (math.sin(math.pi))
