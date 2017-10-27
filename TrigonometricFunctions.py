89641d1292784285bfe2643125bbee23d4a18aa1
OscarOK <oscareom1a@gmail.com>
2017-10-27 15:53:28 -0600

    ADDING SIN FUNCTION
    Using Taylor Serie for sin.

--- TrigonometricFunctions.py
+++ TrigonometricFunctions.py
@@ -9,3 +9,13 @@ def degreesToRad(degrees):
 def radToDegrees(rad):
 
     return (rad * 180) / math.pi
+
+#TAYLOR SERIE OF SIN
+def sin(k, x):
+
+    result = x
+
+    for n in range(1, k):
+        result += ((-1) ** n) / (math.factorial(2 * n + 1)) * (x ** (2 * n + 1))
+
+    return result
