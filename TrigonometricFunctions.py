6d23aadce7e67a6883581590d2e56c041ae8eb03
OscarOK <oscareom1a@gmail.com>
2017-10-27 15:51:14 -0600

    CREATING PY FILE
    Starting the py file with two functions.

---
+++ TrigonometricFunctions.py
@@ -0,0 +1,11 @@
+import math
+
+#PASS FROM DEGREES TO RADIANS
+def degreesToRad(degrees):
+
+    return (degrees * math.pi) / 180
+
+#PASS FROM RADIANS TO DEGREES
+def radToDegrees(rad):
+
+    return (rad * 180) / math.pi
